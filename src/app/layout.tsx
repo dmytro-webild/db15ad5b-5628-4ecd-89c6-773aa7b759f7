6a56438d59888f743adee1b76c24cb88896dc343
bender <kudin.dima@gmail.com>
2026-05-21 09:30:13 +0000

    Update theme fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ServiceWrapper } from "@/components/ServiceWrapper";
 import { getVisualEditScript } from "@/utils/visual-edit-script";
 import { Montserrat } from "next/font/google";
 import { DM_Sans } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 
 
 
@@ -21,12 +22,13 @@ export const metadata: Metadata = {
 };
 
 
-const dmSans = DM_Sans({
-  variable: "--font-dm-sans",
-  subsets: ["latin"],
-});
+
 const inter = Inter({
   variable: "--font-inter",
+  subsets: ["latin"],
+});
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
@@ -38,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <ServiceWrapper>
-        <body className={`${dmSans.variable} ${inter.variable} antialiased`}>
+        <body className={`${inter.variable} ${openSans.variable} antialiased`}>
           
           {children}
           <script
